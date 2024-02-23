e5f0851f9573f8ad4694b928bda3eac47a1115f8
alifzr31 <alifzakya.r@gmail.com>
2024-02-23 14:59:52 +0700

    first commit

--- lib/app/modules/dashboard/widgets/employee/employee_header.dart
+++ lib/app/modules/dashboard/widgets/employee/employee_header.dart
@@ -11,34 +11,36 @@ class EmployeeHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      elevation: 1,
-      clipBehavior: Clip.antiAlias,
-      borderRadius: const BorderRadius.only(
-        bottomLeft: Radius.circular(15),
-        bottomRight: Radius.circular(15),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(15),
-        child: Column(
-          children: [
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: BaseText(
-                text: 'Employee',
-                size: 20,
-                bold: FontWeight.w600,
+    return Obx(
+      () => Material(
+        elevation: 1,
+        clipBehavior: Clip.antiAlias,
+        borderRadius: const BorderRadius.only(
+          bottomLeft: Radius.circular(15),
+          bottomRight: Radius.circular(15),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            children: [
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: BaseText(
+                  text: 'Employee',
+                  size: 20,
+                  bold: FontWeight.w600,
+                ),
               ),
-            ),
-            const SizedBox(height: 10),
-            if (controller.employeeLoading.isFalse)
-              BaseFormField(
-                hint: 'Search Employee...',
-                prefixIcon: const Icon(MingCute.search_3_line),
-                isOutline: true,
-                onChanged: (value) => controller.findEmployee(value ?? ''),
-              ),
-          ],
+              const SizedBox(height: 10),
+              if (controller.employeeLoading.isFalse)
+                BaseFormField(
+                  hint: 'Search Employee...',
+                  prefixIcon: const Icon(MingCute.search_3_line),
+                  isOutline: true,
+                  onChanged: (value) => controller.findEmployee(value ?? ''),
+                ),
+            ],
+          ),
         ),
       ),
     );
