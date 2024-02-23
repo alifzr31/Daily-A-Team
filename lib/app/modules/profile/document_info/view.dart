ce81d1c47e3f61c0009ee63e034117660fde8cff
alifzr31 <alifzakya.r@gmail.com>
2024-02-23 14:05:35 +0700

    first commit

--- lib/app/modules/profile/document_info/view.dart
+++ lib/app/modules/profile/document_info/view.dart
@@ -1,17 +1,35 @@
 import 'package:dailyateam/app/components/base_appbar.dart';
+import 'package:dailyateam/app/components/base_text.dart';
 import 'package:dailyateam/app/core/values/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class DocumentInfoPage extends StatelessWidget {
   const DocumentInfoPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: BaseAppBar(
+    return Scaffold(
+      appBar: const BaseAppBar(
         bgColor: Colors.white,
         fgColor: navyColor,
         title: 'Documents Info',
+      ),
+      body: Container(
+        height: 30,
+        width: Get.width,
+        color: Colors.red.shade200,
+        padding: const EdgeInsets.symmetric(
+          vertical: 5,
+          horizontal: 15,
+        ),
+        child: Center(
+          child: BaseText(
+            text: 'This page still under maintenance',
+            bold: FontWeight.w600,
+            color: Colors.red.shade800,
+          ),
+        ),
       ),
     );
   }
